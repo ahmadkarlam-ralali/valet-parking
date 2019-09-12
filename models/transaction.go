81deab5380d75cc69500b09a64b8deb1a24fdcfb
Ahmad Karlam <ahmad.karlam@gmail.com>
2019-09-12 11:49:26 +0700

    Remove employee_id from transaction model

--- models/transaction.go
+++ models/transaction.go
@@ -3,16 +3,14 @@ package models
 import "time"
 
 type Transaction struct {
-	ID         uint   `json:"id,primary_key"`
-	Code       string `json:"code"`
-	Slot       Slot
-	Employee   User
-	SlotId     uint      `json:"slot_id"`
-	EmployeeId uint      `json:"employee_id"`
-	PlatNo     string    `json:"name"`
-	Total      uint      `json:"total"`
-	StartAt    time.Time `json:"start_at"`
-	EndAt      time.Time `json:"end_at"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	ID        uint   `json:"id,primary_key"`
+	Code      string `json:"code"`
+	Slot      Slot
+	SlotId    uint      `json:"slot_id"`
+	PlatNo    string    `json:"name"`
+	Total     uint      `json:"total"`
+	StartAt   time.Time `json:"start_at"`
+	EndAt     time.Time `json:"end_at"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
