82607606e3e471afac679b03bf2009c5e5903a34
Ahmad Karlam <ahmad.karlam@gmail.com>
2019-09-13 14:51:08 +0700

    Remove slot on building model

--- models/building.go
+++ models/building.go
@@ -4,7 +4,6 @@ import "time"
 
 type Building struct {
 	ID        uint      `json:"id,primary_key"`
-	Slots     []Slot    `json:"slots"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
